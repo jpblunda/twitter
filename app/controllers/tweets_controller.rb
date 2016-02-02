3048b5c8daf75e521bac2768e4aa8bbeb54061e7
Joseph Blunda <jpblunda@gmail.com>
2016-02-01 20:11:58 -0500

    Saving Tweets to database and strong parameters

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,5 +5,19 @@ class TweetsController < ApplicationController
 
 
     def create
+    	# @tweet = Tweet.new(params[:tweet])
+    	@tweet = Tweet.new(tweet_params)
+    	puts "@tweet before save is #{@tweet.inspect}"
+    	@tweet.save
+    	puts "@tweet after save is #{@tweet.inspect}"
+    	redirect_to new_tweet_path
     end
+
+
+    private
+
+    def tweet_params
+    	params.require(:tweet).permit(:content)
+    end
+
 end
